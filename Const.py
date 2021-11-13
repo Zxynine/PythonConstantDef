c80fa152d6597d4093acd4738e661e3242722bd7
Zxynine <79198508+Zxynine@users.noreply.github.com>
2021-11-13 15:56:22 -0500

    Update to prevent infinite recursion
    Prevents infinite recursion by having a generic object returned instead of a constants identity. The generic allows for comparison still, however it does not have any other function aside from creating the constants themselves. I also gave it a meta class which prevents it from being changed in most cases along side a method that throws an error upon subclassing.

--- Const.py
+++ Const.py
@@ -1,32 +1,39 @@
+class StaticType(type):
+	def __setattr__(*args, **kwargs): raise(TypeError('The Constant class does not support assignment!'))
+	def __getattr__(*args, **kwargs): raise(TypeError('The Constant class does not allow attribute access!'))
+
 #I think i can remove NonInheritable or soemthing but honestly im not keen on changing the code
 #for I am astonished it even functions and i dont know what i did to make it do so.
-def Constant(*types):
-	def TypedConstant(CallCls, *TypeCases):
-		nullInstance = None
-		class IterType(type):
-			def __iter__(cls): return iter([None,])
-			def __next__(self): raise StopIteration
-			def __eq__(null,compare): return isinstance(compare,(type(null), *TypeCases))
-			def __ne__(null,compare): return not isinstance(compare,(type(null), *TypeCases))
-		lambdaIdentity = lambda *args, **kwargs: nullInstance
-		lambdaException = lambda ExceptionMessage: lambda *args, **kwargs: exec("raise(TypeError('"+ExceptionMessage+"'))")
-		class NonInheritable(CallCls, metaclass=IterType):
-			__init__ = lambdaIdentity
-			__new__ = lambdaIdentity
-			__call__ = lambdaIdentity
-			__init_subclass__ = lambdaIdentity
-		nullInstance = type(CallCls.__name__, (NonInheritable, CallCls), {})
-		NonInheritable.__init_subclass__ = lambdaException(f'The parent class "{CallCls.__name__}" does not support inheritance!')
-		IterType.__setattr__ = lambdaException(f'The parent class "{CallCls.__name__}" does not support assignment!')
-		IterType.__getattr__ = lambdaException(f'The parent class "{CallCls.__name__}" does not allow attribute access!')
-		return nullInstance
-	if len(types)==0: return TypedConstant
-	return lambda cls: TypedConstant(cls, *types)
+class Constant(metaclass=StaticType):
+	def __new__(cls, *types):
+		def TypedConstant(CallCls, *TypeCases, default=Constant):
+			nullInstance = None
+			lambdaIdentity = lambda *args, **kwargs: nullInstance
+			lambdaException = lambda ExceptionMessage: lambda *args, **kwargs: exec("raise(TypeError('"+ExceptionMessage+"'))")
+			class IterType(type):
+				def __new__(self,name,bases,attrs,**kwargs): return super().__new__(self,name,bases,attrs,**kwargs)
+				def __next__(self): raise StopIteration
+				def __iter__(cls): return iter([TypeCases[0] if len(TypeCases) != 0 else default,])
+				def __eq__(to,compare): return isinstance(compare,(type(to), *TypeCases)) or compare is default
+				def __ne__(to,compare): return not (isinstance(compare,(type(to), *TypeCases)) or compare is default)
+			class NonInheritable(CallCls):
+				__init__ = lambdaIdentity
+				__new__ = lambdaIdentity
+				__call__ = lambdaIdentity
+			nullInstance = IterType(CallCls.__name__, (NonInheritable, CallCls), {})
+			nullInstance.__init_subclass__ = lambdaException(f'The parent class "{CallCls.__name__}" does not support inheritance!')
+			IterType.__setattr__ = lambdaException(f'The parent class "{CallCls.__name__}" does not support assignment!')
+			IterType.__getattr__ = lambdaException(f'The parent class "{CallCls.__name__}" does not allow attribute access!')
+			return nullInstance
+		if len(types)==0: return TypedConstant
+		return lambda cls: TypedConstant(cls, *types)
+	def __init_subclass__(*args, **kwargs): raise TypeError('The Constant class does not allow inheritance!')
 
 @Constant(type(None))
 class Null:pass
 
 EmptyTypes = (type(None), type(Null))
+
 
 
 
@@ -40,8 +47,8 @@ if __name__ == '__main__':
 	<class '__main__.TypedNull'> <class '__main__.TypedNull'>		<class '__main__.UnTypedNull'> <class '__main__.UnTypedNull'>
 
 	===== Iteration =====
-	None
-	None
+	<class 'NoneType'> <class '__main__.Constant'>
+	<class 'NoneType'> <class '__main__.Constant'>
 
 	===== Own-Type equality =====
 	True False		True False
@@ -56,29 +63,31 @@ if __name__ == '__main__':
 	False False		False False
 
 	===== Non-Keyword access Blocking =====
-	Caught Inheritance Exception
-	Caught Assignment Exception
-	Caught Attribute Access Exception
-	Caught Inheritance Exception
-	Caught Assignment Exception
-	Caught Attribute Access Exception
+	Caught Inheritance Exception | Caught Inheritance Exception
+	Caught Assignment Exception | Caught Assignment Exception
+	Caught Attribute Access Exception | Caught Attribute Access Exception
 
-	===== ::Different Constants Equality:: =====
+	===== Generic Constant Equality =====
+	True True
+
+	===== Different Constants Equality =====
 	False
 
-	[Done] exited with code=0 in 0.097 seconds
+	[Done] exited with code=0 in 0.099 seconds
 	"""
 	def printHeader(headName): print('\n' + '='*5 + ' ' + str(headName) + ' ' + '='*5)
 	def printGroups(typL,typR,utypL,utypR): print(str(typL)+' '+str(typR)+('\t'*2)+str(utypL)+' '+str(utypR))
-
+	from typing import Iterable
 	@Constant()
 	class UnTypedNull:pass
 	
 	@Constant(type(None))
 	class TypedNull:pass
 
+	GENERIC = [i for i in UnTypedNull][0]
+
 	def FlattenIterables(items):
-		if not isinstance(items, Iterable):  yield [items,]
+		if not isinstance(items, Iterable): yield items; return
 		for x in items:
 			if isinstance(items, Iterable):
 				yield from FlattenIterables(x)
@@ -91,9 +100,8 @@ if __name__ == '__main__':
 	printGroups(TypedNull,TypedNull,UnTypedNull,UnTypedNull)
 	printGroups(TypedNull(),TypedNull(),UnTypedNull(),UnTypedNull())
 	printHeader('Iteration')
-	for i in TypedNull(): print(i)
-	for i in UnTypedNull(): print(i)
-	# print(list(FlattenIterables([TypedNull,UnTypedNull])))
+	print(*[i for i in TypedNull], *[i for i in UnTypedNull])
+	print(*list(FlattenIterables([TypedNull, UnTypedNull])))
 	printHeader('Own-Type equality')
 	printGroups(TypedNull == TypedNull, TypedNull != TypedNull, UnTypedNull == UnTypedNull, UnTypedNull != UnTypedNull)
 	printGroups(TypedNull == TypedNull(), TypedNull() != TypedNull, UnTypedNull == UnTypedNull(), UnTypedNull() != UnTypedNull)
@@ -104,17 +112,23 @@ if __name__ == '__main__':
 	printHeader('Wrong-Type equality')
 	printGroups(TypedNull == object, TypedNull == type, UnTypedNull == object, UnTypedNull == type)
 	printHeader('Non-Keyword access Blocking')
-	try: type('Inherit', (TypedNull,),{})
-	except TypeError: print('Caught Inheritance Exception')
-	try: TypedNull.N = None
-	except TypeError: print('Caught Assignment Exception')
-	try: TypedNull.N
-	except TypeError: print('Caught Attribute Access Exception')
-	try: type('Inherit', (UnTypedNull,),{})
-	except TypeError: print('Caught Inheritance Exception')
-	try: UnTypedNull.N = None
-	except TypeError: print('Caught Assignment Exception')
-	try: UnTypedNull.N
-	except TypeError: print('Caught Attribute Access Exception')
-	printHeader('::Different Constants Equality::')
+	inheritance, assignment, attrAccess= [None,None], [None,None], [None,None]
+	try: inheritance[0] = type('Inherit', (TypedNull,),{})
+	except TypeError: inheritance[0] = ('Caught Inheritance Exception')
+	try: assignment[0] = TypedNull.N = None
+	except TypeError: assignment[0] = ('Caught Assignment Exception')
+	try: attrAccess[0] = TypedNull.N
+	except TypeError: attrAccess[0] = ('Caught Attribute Access Exception')
+	try: inheritance[1] = type('Inherit', (UnTypedNull,),{})
+	except TypeError: inheritance[1] = ('Caught Inheritance Exception')
+	try: assignment[1] = UnTypedNull.N = None
+	except TypeError: assignment[1] = ('Caught Assignment Exception')
+	try: attrAccess[1] = UnTypedNull.N
+	except TypeError: attrAccess[1] = ('Caught Attribute Access Exception')
+	print(*inheritance, sep= ' | ')
+	print(*assignment, sep= ' | ')
+	print(*attrAccess, sep= ' | ')
+	printHeader('Generic Constant Equality')
+	print(UnTypedNull == GENERIC, TypedNull == GENERIC)
+	printHeader('Different Constants Equality')
 	print(UnTypedNull == TypedNull)
